bb841d605d0520f55b92b7eada8c45e582fb09ca
phvohieunghia <phvohieunghia@gmail.com>
2022-03-25 16:27:54 +0700

    to develop footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import dataList from "../../assets/data/Footer.json";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="ft__title">{dataList.title1}</div>
+      <div className="ft__content">
+        <div className="ft__text">
+          {dataList.content.map((data, i) => {
+            return data;
+          })}
+        </div>
+      </div>
+      <div className="ft__title">{dataList.title2}</div>
+      <div className="ft__logo">
+        {dataList.img.map((data, i) => {
+          return (
+            <div className="ft__logo__item">
+              <img src={"/img/footer/" + data} alt="error" />
+            </div>
+          );
+        })}
+      </div>
+      <div className="ft__downloadLogo">
+        {dataList.downloadImg.map((data, i) => {
+          return (
+            <div className="ft__downloadLogo__item">
+              <img src={"/img/footer/" + data} alt="error" />
+            </div>
+          );
+        })}
+      </div>
+      <div className="ft__support">
+        <div className="ft__support__left">
+          <img src={"/img/footer/" + dataList.support.img} alt="error png" />
+        </div>
+        <div className="ft__support__right">
+          <div className="ft__support__right--title">
+            {dataList.support.title}
+          </div>
+          <div className="ft__support__right--title">
+            {dataList.support.phone}
+          </div>
+        </div>
+      </div>
+      <div className="ft__email">
+        <div className="ft__email-description">{dataList.description}</div>
+        <div className="ft__email-form">
+          <div className="ft__email-form__input">
+            <input type="text" placeholder="Nhập email của bạn ..." />
+          </div>
+          <div className="ft__email-form__icon">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
